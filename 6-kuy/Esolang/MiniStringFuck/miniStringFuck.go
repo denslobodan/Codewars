615ef6db83f1f99a9bec4cea3fc2ef1f7755458d
Denis Viktorovich <slbdenis.victor@gmail.com>
2023-06-27 21:08:29 +0300

    Update interpreter

--- 6-kuy/Esolang/MiniStringFuck/miniStringFuck.go
+++ 6-kuy/Esolang/MiniStringFuck/miniStringFuck.go
@@ -1,16 +1,53 @@
 package ministingfuck
 
+import (
+	"bufio"
+	"os"
+)
+
 func Interpreter(code string) string {
-	memory := make([]int, 60000)
-	ptr := 0
-	s := ""
+	var memory = make([]int, 1000)
+	var ptr int
+	var output string
+
 	for i := 0; i < len(code); i++ {
 		switch code[i] {
 		case '+':
 			memory[ptr]++
+		case '-':
+			memory[ptr]--
+		case ',':
+			reader := bufio.NewReader(os.Stdin)
+			char, _, err := reader.ReadRune()
+			if err != nil {
+				// handle error
+			}
+			memory[ptr] = int(char)
 		case '.':
-			s += string(rune(memory[ptr]))
+			output += string(rune(memory[ptr]))
+		case '>':
+			ptr++
+		case '<':
+			ptr--
+		case '[':
+			if memory[ptr] == 0 {
+				for j := i; j < len(code); j++ {
+					if code[j] == ']' {
+						i = j
+						break
+					}
+				}
+			}
+		case ']':
+			if memory[ptr] != 0 {
+				for j := i; j >= 0; j-- {
+					if code[j] == '[' {
+						i = j
+						break
+					}
+				}
+			}
 		}
 	}
-	return s
+	return output
 }
